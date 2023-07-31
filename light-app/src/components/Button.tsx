5c12594753c8c44162a06f37e6a92d2fb8404431
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-07-31 12:56:22 -0400

    Add files via upload

---
+++ light-app/src/components/Button.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable-next-line no-restricted-imports -- only use with typesafe button routes */
+import { Button as ButtonBase } from "@lightsparkdev/ui/components";
+import { type RoutesType } from "../routes";
+
+export const Button = ButtonBase<RoutesType>;
---
+++ light-app/src/pages/DashboardPage.tsx
@@ -0,0 +1,155 @@
+import styled from "@emotion/styled";
+import { b64encode, DefaultCrypto, KeyOrAlias } from "@lightsparkdev/core";
+import { useJwtAuth, useLightsparkClient } from "@lightsparkdev/react-wallet";
+import {
+  KeyType,
+  WalletDashboard,
+  WalletStatus,
+} from "@lightsparkdev/wallet-sdk";
+import { useEffect, useState } from "react";
+import { useNavigate } from "src/common/router";
+import { Button } from "src/components/Button";
+import { Routes } from "src/routes";
+import RequireAuth from "../auth/RequireAuth";
+import Dashboard from "../components/Dashboard";
+import useWalletInfo from "../hooks/useWalletInfo";
+
+function DashboardPage() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const { wallet, refresh: refreshWallet } = useWalletInfo();
+  const clientProvider = useLightsparkClient();
+  const [dashboard, setDashboard] = useState<WalletDashboard>();
+  const auth = useJwtAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!wallet) {
+      return;
+    }
+    const client = clientProvider.getClient();
+    client
+      .getWalletDashboard()
+      .then((dashboard) => {
+        if (dashboard) {
+          setDashboard(dashboard);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [wallet, clientProvider]);
+
+  let content;
+  if (loading) {
+    content = <div>Loading...</div>;
+  } else if (!dashboard || dashboard.status === WalletStatus.FAILED) {
+    content = <div>Something went wrong :-(</div>;
+  } else if (
+    dashboard.status === WalletStatus.NOT_SETUP ||
+    dashboard.status === WalletStatus.DEPLOYING
+  ) {
+    content = (
+      <DeployWallet
+        status={wallet?.status || WalletStatus.FAILED}
+        onDeploy={async () => {
+          setLoading(true);
+          await clientProvider.getClient().deployWalletAndAwaitDeployed();
+          await refreshWallet();
+          setLoading(false);
+        }}
+      />
+    );
+  } else if (
+    dashboard.status === WalletStatus.DEPLOYED ||
+    dashboard.status === WalletStatus.INITIALIZING
+  ) {
+    content = (
+      <InitializeWallet
+        status={wallet?.status || WalletStatus.FAILED}
+        onInitialize={async () => {
+          setLoading(true);
+          const keys = await DefaultCrypto.generateSigningKeyPair();
+          const serializedPublicKeyBytes =
+            await DefaultCrypto.serializeSigningKey(keys.publicKey, "spki");
+          const serializedPrivateKeyBytes =
+            await DefaultCrypto.serializeSigningKey(keys.privateKey, "pkcs8");
+          // You can save the keys somewhere here if you want to use them later. Here,
+          // we're just logging them to the console for demo purposes.
+          console.log("Save these keys if you want to use this wallet later!");
+          console.log("Public key:", b64encode(serializedPublicKeyBytes));
+          console.log("Private key:", b64encode(serializedPrivateKeyBytes));
+          await clientProvider
+            .getClient()
+            .initializeWalletAndAwaitReady(
+              KeyType.RSA_OAEP,
+              b64encode(serializedPublicKeyBytes),
+              KeyOrAlias.key(b64encode(serializedPrivateKeyBytes))
+            );
+          await refreshWallet();
+          setLoading(false);
+        }}
+      />
+    );
+  } else {
+    content = (
+      <div>
+        <Header>
+          <Button
+            text="Sign out"
+            primary
+            onClick={async () => {
+              await auth.logout();
+              navigate(Routes.Login);
+            }}
+          />
+        </Header>
+        <Dashboard data={dashboard} />
+      </div>
+    );
+  }
+
+  return <RequireAuth>{content}</RequireAuth>;
+}
+
+const DeployWallet = ({
+  onDeploy,
+  status,
+}: {
+  onDeploy: () => void;
+  status: WalletStatus;
+}) => {
+  return (
+    <div>
+      <h1>Wallet not yet deployed. Status: {status}</h1>
+      <Button primary onClick={onDeploy} text="Deploy Wallet" />
+    </div>
+  );
+};
+
+const InitializeWallet = ({
+  onInitialize,
+  status,
+}: {
+  onInitialize: () => void;
+  status: WalletStatus;
+}) => {
+  return (
+    <div>
+      <h1>Wallet not yet initialized. Status: {status}</h1>
+      <Button primary onClick={onInitialize} text="Initialize Wallet" />
+    </div>
+  );
+};
+
+const Header = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  width: 100%;
+  padding: 16px;
+`;
+
+export default DashboardPage;
